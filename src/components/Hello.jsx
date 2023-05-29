d2b7465fa4f41114c52a09d3c794a2472cd3ee0c
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-05-29 17:57:44 +0900

    new component Hello

---
+++ src/components/Hello.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+function Hello() {
+  return (
+    <View>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 16,
+  },
+});
+
+export default Hello;
