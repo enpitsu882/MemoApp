a307b8538f2cf2a67f439422213f0bbb9c3878bb
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-05-30 20:23:02 +0900

    Extract components

--- App.jsx
+++ App.jsx
@@ -1,22 +1,7 @@
-import { StyleSheet, View } from 'react-native';
-
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    <MemoListScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f4f8',
-  },
-});
